f7b91651d8727c9af2fcc3edd39b2562802582fd
Azziden <azzide.rif@gmail.com>
2022-05-31 16:32:50 +0200

    Registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,17 +14,30 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
-    {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
-        ]);
-    }
-
     private $passwordEncoder;
 
         public function __construct(UserPasswordHasherInterface $passwordEncoder){
             $this->passwordEncoder = $passwordEncoder;
         }
-}
+    
+        #[Route('/registration', name: 'app_registration')]
+        public function index(Request $request, ManagerRegistry $doctrine): Response
+        {
+            $user = new User();
+            $form = $this->createForm(UserType::class, $user);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()){
+                $user->setPassword($this->passwordEncoder->hashPassword($user, $user->getPassword()));
+                $user->setRoles(['ROLE_USER']);
+                $em = $doctrine->getManager();
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('Success', 'Vous êtes enregistré ');
+                Return $this->redirectToRoute('app_login');
+            }
+            return $this->render('registration/registration.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+    }
