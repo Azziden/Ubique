1709dfa7a2d1703a52600c10dfb2f509afe752fa
Azziden <azzide.rif@gmail.com>
2022-05-31 15:02:11 +0200

    first commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration')]
+    public function index(): Response
+    {
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+
+    private $passwordEncoder;
+
+        public function __construct(UserPasswordHasherInterface $passwordEncoder){
+            $this->passwordEncoder = $passwordEncoder;
+        }
+}
