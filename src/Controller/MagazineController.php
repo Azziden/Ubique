d2e18430a5c55b7524f063346bbe758cd4167a47
Azziden <azzide.rif@gmail.com>
2022-08-09 23:48:37 +0200

    add: Missing files
    Signed-off-by: Azziden <azzide.rif@gmail.com>

---
+++ src/Controller/MagazineController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Magazine;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MagazineController extends AbstractController
+{
+    #[Route('/magazine/{magazine}', name: 'app_choose_magazine_type')]
+    public function view(Magazine $magazine, Request $request, ManagerRegistry $doctrine): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        //Enregistrer date de parution dans bdd
+        $entityManager = $doctrine->getManager();
+
+        $now = date_create();
+
+        $date = $request->get('date_de_parution');
+        if ($date) {
+            if ($magazine->getDateDeParutionSetAt() === null || $magazine->getDateDeParutionSetAt()->diff($now)->days < 5) {
+                $magazine->setDateDeParution($date);
+
+                if ($magazine->getDateDeParutionSetAt() === null) {
+                    $magazine->setDateDeParutionSetAt($now);
+                }
+
+                $entityManager->flush();
+            } else {
+                $this->addFlash('danger', "Ce n'est pas possible de modifier la date de parution");
+            }
+        }
+
+        $date = $request->get('date_de_bouclage');
+        if ($date) {
+            if ($magazine->getDateDeBouclageSetAt() === null || $magazine->getDateDeBouclageSetAt()->diff($now)->days < 5) {
+                $magazine->setDateDeBouclage($date);
+
+                if ($magazine->getDateDeBouclageSetAt() === null) {
+                    $magazine->setDateDeBouclageSetAt($now);
+                }
+
+                $entityManager->flush();
+            } else {
+                $this->addFlash('danger', "Ce n'est pas possible de modifier la date de bouclage");
+            }
+        }
+
+
+        return $this->render('magazine/view.html.twig', [
+            'controller_name' => 'ChooseMagazineTypeController',
+            'magazine' => $magazine,
+        ]);
+    }
+}
